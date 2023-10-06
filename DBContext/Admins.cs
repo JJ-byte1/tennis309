008ae18cc96e03016f8a902519360fdccc6bd90e
JJ-Byte1 <s356169@students.cdu.edu.au>
2023-10-06 10:51:52 +0930

    clean project files attempt 2

---
+++ DBContext/Admins.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Groupweb.DBContext
+{
+    public class Admins
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+
+    }
+}
